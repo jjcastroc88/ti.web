d8cb2ea177cb13dc5a3b82bfb875b7cac97259ff
Jose Castro <jjcastroc88@gmail.com>
2019-03-17 01:14:56 -0500

    Initial config

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+import { Provider } from 'react-redux';
+import { NavigationDrawer } from 'react-md';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <NavigationDrawer
+        drawerTitle="react-md with CRA"
+        toolbarTitle="Welcome to react-md"
+      >
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome as to React 123</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        </div>
+      </NavigationDrawer>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
